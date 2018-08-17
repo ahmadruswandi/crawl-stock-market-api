d8dcfc3f0cfad071ca83f6cbc3b9b6229d255b27
unknown <kaisansoft@gmail.com>
2018-08-17 07:58:30 +0700

    Saved data to json. Created models

--- crawl/engine/market-stock.py
+++ crawl/engine/market-stock.py
@@ -1,3 +1,4 @@
+import json
 from io import BytesIO
 
 from bs4 import BeautifulSoup as BSoup, Tag, NavigableString
@@ -29,6 +30,7 @@ class MarketStockEngine:
         data = data.decode("utf-8")
         bs = BSoup(data, 'html.parser')
         cnt = 0
+        datalist = []
         for tag in bs.find_all("p", "r1"):
             for tr in tag.find_all("tr"):
                 tds = tr.find_all("td")
@@ -44,7 +46,6 @@ class MarketStockEngine:
                     ticker_symbol = '-'
                     url = '-'
 
-
                 data = {
                     'ticker symbol': ticker_symbol,
                     'url': url,
@@ -52,11 +53,15 @@ class MarketStockEngine:
                     'business': self.extract_content(tds[2]),
                     'Listing bourse': self.extract_content(tds[3])
                 }
-                print(data)
+                datalist.append(data)
+                # print(data)
 
-                cnt += 1
-                if cnt > 3:
-                    break
+                # cnt += 1
+                # if cnt > 3:
+                #     break
+
+        with open('company_index.json', 'w', encoding='utf-8') as f:
+            json.dump(datalist, f, ensure_ascii=False)
 
     def fetch_company_info(self, url):
         buffer = BytesIO()
@@ -77,21 +82,13 @@ class MarketStockEngine:
         # auditing_tr = main_tbl.find_all("tr")[1]
         # auditing_td = auditing_tr.find_all("td")[0]
 
+        seglist = self.get_segment_list(main_tbl)
+        auditing_company = self.get_info_segment(seglist, "auditing company")
+        business_registration = self.get_info_segment(seglist, "business registration")
+        print("auditing_company", auditing_company)
+        print()
+        print("business_registration", business_registration)
 
-        clist = []
-        for td in main_tbl.find_all('td'):
-            colspan = td.get("colspan")
-            if str(colspan) == '2':
-                print(td)
-                for c in td.contents:
-                    text = str(c).strip()
-                    if text.lower() == '<br/>':
-                        continue
-
-                    clist.append(text)
-
-        for idx, c in enumerate(clist):
-            print(idx, c)
 
         clist = []
         for c in company_profile_td.contents:
@@ -142,27 +139,55 @@ class MarketStockEngine:
                 if cnt > 3:
                     break
 
+    def get_segment_list(self, main_tbl):
+        seglist = []
+        for td in main_tbl.find_all('td'):
+            colspan = td.get("colspan")
+            if str(colspan) == '2':
+                for c in td.contents:
+                    text = str(c).strip()
+                    if text.lower() == '<br/>':
+                        continue
+
+                    seglist.append(text)
+
+        return seglist
+
+    def get_info_segment(self, seglist, segment_text):
+
+        b_segment = False
+        segment_arr = []
+        for idx, c in enumerate(seglist):
+            if c.lower().__contains__("<strong>"):
+                b_segment = c.lower().__contains__(segment_text)
+
+            if b_segment:
+                segment_arr.append(c)
+
+        return segment_arr[1:]
 
     def extract_content(self, td_obj):
         if td_obj:
-            return td_obj.contents[0]
+            return str(td_obj.contents[0])
         else:
             return '-'
 
 
 mse = MarketStockEngine()
+mse.fetch_company_list()
 # t = Tag()
 
-url = 'http://stock.vietnammarkets.com/others/ANV/'
-mse.fetch_company_info(url)
-print("*" * 50)
-print()
-url = 'http://stock.vietnammarkets.com/electric-and-electronics-equipment/VBH/'
-mse.fetch_company_info(url)
-print("*" * 50)
-print()
-url = 'http://stock.vietnammarkets.com/paper-and-wood-products/ILC/'
-mse.fetch_company_info(url)
+# url = 'http://stock.vietnammarkets.com/others/ANV/'
+# mse.fetch_company_info(url)
+# print("*" * 50)
+# print()
+# url = 'http://stock.vietnammarkets.com/electric-and-electronics-equipment/VBH/'
+# mse.fetch_company_info(url)
+# print("*" * 50)
+# print()
+# url = 'http://stock.vietnammarkets.com/paper-and-wood-products/ILC/'
+# url = 'http://stock.vietnammarkets.com/communication/EBS/'
+# mse.fetch_company_info(url)
 
 # bs = mse.fetch_company_list()
 # print("fetch_company_list", "FINISHED")
--- crawl/models.py
+++ crawl/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    ticker_symbol = models.CharField(max_length=6, unique=True)
+    name = models.CharField(max_length=50)
+    url = models.CharField(max_length=50)
+    business = models.CharField(max_length=50)
+    listing_bourse = models.CharField(max_length=10)
+    crawled_at = models.DateTimeField(null=False, auto_created=True, auto_now=True)
+
+
